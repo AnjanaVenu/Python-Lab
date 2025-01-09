088f58e4791798e8aff07ca7059d5fe225edd7e1
AnjanaVenu <150990231+AnjanaVenu@users.noreply.github.com>
2025-01-09 22:52:29 +0530

    lab programs

--- pythonCO1/q5.py
+++ pythonCO1/q5.py
@@ -1,3 +1,5 @@
+# list of integers for all values greater than 100 store 'over' instead use list comprehension.
+
 u = input("Enter the list of integers separated by spaces:") 
 result=['over' if int(num) > 100 else int(num) for num in u.split()]
 print(result)
