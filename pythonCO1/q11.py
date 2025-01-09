66a3c33cf84ab32d510068ad1725382affc64d93
AnjanaVenu <150990231+AnjanaVenu@users.noreply.github.com>
2025-01-09 23:01:41 +0530

    lab programs

---
+++ pythonCO1/q11.py
@@ -0,0 +1,11 @@
+# Find biggest of 3 numbers entered
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
+if a>b and a>c:
+ print("a is greater than b and c")
+elif b>a and b>c:
+ print("b is greater than a and c")
+else:
+ print("c is greater than a and b")
