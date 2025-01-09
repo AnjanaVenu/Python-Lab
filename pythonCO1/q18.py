8a6fec9d4b93558698d861a0d67126ec953c4add
AnjanaVenu <150990231+AnjanaVenu@users.noreply.github.com>
2025-01-09 23:07:12 +0530

    lab problems

---
+++ pythonCO1/q18.py
@@ -0,0 +1,8 @@
+# Merge two dictionaries
+
+d1 = {}
+d2 = {}
+for i in range(int(input("Enter number of items for d1: "))):
+key = input("Enter key: ")
+value = int(input("Enter value: "))
+d1[key] = value
