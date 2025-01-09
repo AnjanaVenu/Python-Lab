fb18089715765e78cbf3c83a64c2a5dd75a6903d
AnjanaVenu <150990231+AnjanaVenu@users.noreply.github.com>
2025-01-09 22:38:50 +0530

    lab programs
    Display future leap years from current year to a final year

---
+++ pythonCO1/q2.py
@@ -0,0 +1,6 @@
+yr1 = int(input("Enter first year:"))
+yr2 = int(input("Enter second year:"))
+print("Leap year between",yr1,"and",yr2,"is")
+for i in range(yr1,yr2+1):
+ if (i%4==0 and i%100!=0) or (i%400==0):
+   print(i)
