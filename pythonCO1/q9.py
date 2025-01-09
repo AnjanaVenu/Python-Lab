4ce6b64a92a06b11dd866bfb84b660f015234d98
AnjanaVenu <150990231+AnjanaVenu@users.noreply.github.com>
2025-01-09 22:59:59 +0530

    lab programs

---
+++ pythonCO1/q9.py
@@ -0,0 +1,5 @@
+# Create a string from given string where first and last characters exchanged
+
+s=input("Enter a string:")
+s2=s[-1]+s[1:-1]+s[0]
+print("String after first and last characters exchanged:",s2)
