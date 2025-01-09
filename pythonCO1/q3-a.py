7fcc0da2dacd54bc42029829ff5ffc7e939b2e85
AnjanaVenu <150990231+AnjanaVenu@users.noreply.github.com>
2025-01-09 22:49:45 +0530

    lab programs

--- pythonCO1/q3-a.py
+++ pythonCO1/q3-a.py
@@ -1,3 +1,5 @@
+# Generate positive list of numbers from a given list of integers
+
 l = [int(i) for i in input("Enter elements: ").split()]
 l_p = [i for i in l if i>=0]
 print("List of +ve numbers: " , l_p)
