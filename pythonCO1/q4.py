3ad387b379775bb8fb9e1fdadff5457218af7b86
AnjanaVenu <150990231+AnjanaVenu@users.noreply.github.com>
2025-01-09 22:51:51 +0530

    lab programs

--- pythonCO1/q4.py
+++ pythonCO1/q4.py
@@ -1,3 +1,5 @@
+# Count the occurrences of each word in a line of text.
+
 text=[word for word in input("Enter a text:").split()]
 count=[(word, text.count(word)) for word in set(text)]
 print("Word occurrences:")
